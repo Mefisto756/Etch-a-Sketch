bd509bbe7e4630b860abb2ca8ba521f509920940
Your Name <thepolaszek@gmail.com>
2024-02-25 21:42:51 +0100

    set maximu size to 100

--- script.js
+++ script.js
@@ -17,16 +17,19 @@ function resetGrid() {
 function generateGrid() {
     deleteGrid();
     const size = document.querySelector("#grid").value;
-    const container = document.querySelector("#container");
-
-    container.style.width = (size * 50) + 'px';
-    for(let i = 0; i < size **2; i++) {
-        const div = document.createElement('div');
-        div.style.background = "white";
-        div.style.width = "50px";
-        div.style.height = "50px";
-        div.addEventListener('mouseover', () => {div.style.background = "black";})
-        container.appendChild(div);
+    if (size > 100) {
+        alert("Size 100 is maximum!!")
+    } else {
+        const container = document.querySelector("#container");
+        let subDivSize = 800 / size;
+        for(let i = 0; i < size **2; i++) {
+            const div = document.createElement('div');
+            div.style.background = "white";
+            div.style.width = subDivSize + 'px';
+            div.style.height = subDivSize + 'px';
+            div.addEventListener('mouseover', () => {div.style.background = "black";})
+            container.appendChild(div);
+        }
     }
 }
 
