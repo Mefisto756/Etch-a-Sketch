ca5b009a17eb18a9646771f51f264ec081106ee7
Your Name <thepolaszek@gmail.com>
2024-02-25 19:07:22 +0100

    add js functionality to buttons

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const container = document.querySelector("#container");
-for(let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.style.background = "white";
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.addEventListener('mouseover', () => {div.style.background = "black";})
-    container.appendChild(div);
+function deleteGrid() {
+    const container = document.querySelector("#container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
 }
+
+function resetGrid() {
+    const container = document.querySelector("#container");
+    const children = container.querySelectorAll("#container div");
+    children.forEach(element => {
+        element.style.background = "white";
+        });
+    
+}
+
+function generateGrid() {
+    deleteGrid();
+    const size = document.querySelector("#grid").value;
+    const container = document.querySelector("#container");
+
+    container.style.width = (size * 50) + 'px';
+    for(let i = 0; i < size **2; i++) {
+        const div = document.createElement('div');
+        div.style.background = "white";
+        div.style.width = "50px";
+        div.style.height = "50px";
+        div.addEventListener('mouseover', () => {div.style.background = "black";})
+        container.appendChild(div);
+    }
+}
+
+const apply = document.querySelector("#apply");
+apply.addEventListener('click', () => generateGrid());
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', () => resetGrid());
